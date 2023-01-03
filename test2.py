b6f5dec29ccb60779224423501efa6366af38246
Vitalii Hryhorchuk <gvit84@gmail.com>
2023-01-03 15:24:42 +0200

    third commit deleted test2.py

--- test2.py
+++
@@ -1,9 +0,0 @@
-main_dct = {}
-def many_dct(**kwargs):
-    main_dct.update(**kwargs)
-    return main_dct
-many_dct(brand='Volvo', model='XC90', year=2022)
-many_dct(sedan='Volkswagen', Coupe='Porsche')
-many_dct(January='spring', June='summer')
-many_dct(name='Vitalii', age=38, eyes_color='green')
-print(main_dct)
